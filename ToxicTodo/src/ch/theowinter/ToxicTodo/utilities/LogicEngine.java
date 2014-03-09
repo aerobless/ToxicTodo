8e088b2efd6c6bec1c9f7a68857e8af1128716b6
aerobless <theo.winter@gmail.com>
2014-03-09 16:16:17 +0100

    Added Logic Engine, to concatenate array to string

---
+++ ToxicTodo/src/ch/theowinter/ToxicTodo/utilities/LogicEngine.java
@@ -0,0 +1,37 @@
+package ch.theowinter.ToxicTodo.utilities;
+
+public class LogicEngine {
+
+    /**
+     * Concatenates an array from startPosition to the end of the array.
+     * Originally built for my minecraft plugin "BloodAST"
+     * @param args
+     * @param arrayLength
+     * @return concatenatedArray
+     */    
+    public String[] concatenateArgs(String[] args, int arrayLength){
+    	int startPosition = arrayLength-1;
+      	String[] concatenatedArray = new String[arrayLength];
+    	if (args.length>arrayLength){
+	    	StringBuilder builder = new StringBuilder();
+	    	//Append all args from the startPosition to the end of the array, -1 because Array starts at 0
+	    	for(int i = startPosition; i<args.length;i++){
+	    		builder.append(args[i]);
+	    		if(i < (args.length-1)){
+	    			builder.append(" ");
+	    		}
+	    	}
+	    	//Copy args that weren't concatenated into the new array
+	    	for(int i=0; i<concatenatedArray.length; i++){
+	    		concatenatedArray[i]=args[i];
+	    	}
+	    	//Replace last arg with our newly built string, -1 because Array starts at 0
+	    	concatenatedArray[startPosition]=builder.toString();
+    	}
+    	else{
+    		concatenatedArray = args;
+    	}
+		return concatenatedArray;
+    }
+	
+}
