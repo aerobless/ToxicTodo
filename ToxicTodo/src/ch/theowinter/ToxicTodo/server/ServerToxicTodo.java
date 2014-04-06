a9596492cbeb1538afa0f63045321ea7aebe072c
aerobless <theo.winter@gmail.com>
2014-04-06 12:53:27 +0200

    something is very wrong.. the server doesn't run properly on the pi anymore.. but locally it runs just fine.. ffs WHY

--- ToxicTodo/src/ch/theowinter/ToxicTodo/server/ServerToxicTodo.java
+++ ToxicTodo/src/ch/theowinter/ToxicTodo/server/ServerToxicTodo.java
@@ -32,7 +32,7 @@ public class ServerToxicTodo {
 	private static Semaphore stopServer = new Semaphore(1);
 
 	public static void main(String[] args) { 
-		//Load sample data or stored data
+		//Load sample data or stored data 
 		loadSettings();
 		
 		//Open up a connection:
